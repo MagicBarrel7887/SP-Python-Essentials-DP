65166aa942829ca53e9850a4a72b404b298ce21e
nate <costerboy03@outlook.com>
2022-02-09 17:17:00 -0600

    2 labs

---
+++ Module 2/2.1.1.18_LAB_The_print()_function.py
@@ -0,0 +1,16 @@
+# Python 3.10.2
+
+#Objectives
+#becoming familiar with the print() function and its formatting capabilities
+#experimenting with Python code.
+#Scenario
+#Modify the first line of code in the editor, using the sep and end keywords, to match the expected output. Use the two print() functions in the editor.
+
+#Don't change anything in the second print() invocation.
+
+#Expected output
+#Programming***Essentials*** in ...Python
+#output
+
+print("Programming", "Essentials", "in", sep ="***", end ="...")
+print("Python")
