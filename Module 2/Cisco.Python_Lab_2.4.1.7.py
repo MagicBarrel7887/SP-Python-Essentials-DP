5e3a13aeb508a12a653a00a859d8a207433c1f97
MagicBarrel7887 <90459450+MagicBarrel7887@users.noreply.github.com>
2022-02-11 11:08:48 -0600

    Cisco.Python_Lab_2.4.1.7

---
+++ Module 2/Cisco.Python_Lab_2.4.1.7.py
@@ -0,0 +1,11 @@
+john = 3
+mary = 5
+adam = 6
+
+print(john, mary, adam, sep=",", end="\n")
+
+total_apples =+ john
+total_apples =+ mary
+total_apples =+ adam
+
+print("total_apples:", total_apples, end="\n")
