1b121b07e7f0d307d91813d88863c8f8d41f5ae4
nate <costerboy03@outlook.com>
2022-03-16 22:23:51 -0500

    Create 3.6.1.9 LAB Operating with lists.py

---
+++ Module 3/3.6.1.9 LAB Operating with lists.py
@@ -0,0 +1,25 @@
+from re import T
+
+
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+temp_list = my_list
+
+
+
+print(temp_list)
+
+for i in temp_list:
+    tem_int = temp_list[i]
+    for j in temp_list:
+        if tem_int == temp_list[j]:
+            print(tem_int,temp_list[j] ,sep=" : ")
+            temp_list.pop(1-j)
+            pass
+        pass
+    pass
+print(temp_list)
+#
+# Write your code here.
+#
+print("The list with unique elements only:")
+print(my_list)
