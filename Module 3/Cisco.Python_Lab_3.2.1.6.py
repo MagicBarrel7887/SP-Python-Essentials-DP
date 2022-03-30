f70c4d877e898f428e181bd21b9d1b670ecfe5f6
nate <costerboy03@outlook.com>
2022-03-29 22:21:30 -0500

    Create Cisco.Python_Lab_3.2.1.6.py

---
+++ Module 3/Cisco.Python_Lab_3.2.1.6.py
@@ -0,0 +1,15 @@
+from cgi import print_form
+import time
+print("Ready or not, here I come!", end="\n")
+print("", end="\n")
+time.sleep(1)
+
+for item in range(1,6):
+    print(item, "Mississippi", end="\n")
+    time.sleep(1)
+ 
+# Write a for loop that counts to five.
+# Body of the loop - print the loop iteration number and the word "Mississippi".
+# Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
