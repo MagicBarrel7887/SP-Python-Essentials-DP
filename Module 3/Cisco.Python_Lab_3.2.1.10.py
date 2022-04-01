a3878b6498a19e2a5fd2a319cab83b0b3b969c4a
nate <costerboy03@outlook.com>
2022-03-31 22:26:26 -0500

    Create Cisco.Python_Lab_3.2.1.10.py

---
+++ Module 3/Cisco.Python_Lab_3.2.1.10.py
@@ -0,0 +1,22 @@
+user_word = input()
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A":
+      pass
+    elif letter == "E":
+      pass
+    elif letter == "I":
+      pass
+    elif letter == "O":
+      pass
+    elif letter == "U":
+      pass
+    else:
+        print(letter)
+    continue
+    
+    
+    # Complete the body of the for loop.
