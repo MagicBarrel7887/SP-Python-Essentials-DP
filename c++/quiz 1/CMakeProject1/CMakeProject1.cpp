27f96603a2b49e57b50e075a0907dc093aa2b2d3
nate <costerboy03@outlook.com>
2022-04-05 21:18:47 -0500

    added cpp

---
+++ c++/quiz 1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,28 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+tests test;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	
+	test.myMethod();
+
+	test.testq2();
+
+	test.testq3();
+
+	test.testq4(10);
+	test.testq4(3.0);
+	test.testq6(2.0);
+	
+	return 0;
+}
+
+// C++ Essentials - part 1 :: Module 1 QUIZ 90%
