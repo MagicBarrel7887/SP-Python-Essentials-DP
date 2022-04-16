242691db0b8ff110de98dba504601edfc5fc72d7
nate <costerboy03@outlook.com>
2022-04-16 16:45:05 -0500

    FIX Trailing newlines

--- Module 4/Python.Lab.4.3.1.10.py
+++ Module 4/Python.Lab.4.3.1.10.py
@@ -24,12 +24,12 @@ def miles_gallon_to_liters_100km(miles):
 
  Liters = us / pre
 
- print("NOT DATA")
+
  return Liters
+
 print(liters_100km_to_miles_gallon(3.9))
 print(liters_100km_to_miles_gallon(7.5))
 print(liters_100km_to_miles_gallon(10.))
 print(miles_gallon_to_liters_100km(60.3))
 print(miles_gallon_to_liters_100km(31.4))
 print(miles_gallon_to_liters_100km(23.5))
-
