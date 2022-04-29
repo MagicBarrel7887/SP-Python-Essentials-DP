c2585b61a3a1e45674c133f73318381323cf4bb2
nate <costerboy03@outlook.com>
2022-04-29 11:39:42 -0500

    quiz mod 2 start

---
+++ c++/module-2-quiz-2/module-2-quiz-2.cpp
@@ -0,0 +1,17 @@
+﻿// module-2-quiz-2.cpp : Defines the entry point for the application.
+//
+
+#include "module-2-quiz-2.h"
+
+using namespace std;
+quiz qz;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << qz.quiz1() << endl;
+	qz.quiz2();
+		
+	return 0;
+}
+
