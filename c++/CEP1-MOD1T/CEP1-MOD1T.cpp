be2ca0a29816080b730baf4f3243a59ff65b6bc6
nate <costerboy03@outlook.com>
2022-05-13 21:53:32 -0500

    CEP1-MOD1T

---
+++ c++/CEP1-MOD1T/CEP1-MOD1T.cpp
@@ -0,0 +1,12 @@
+﻿// CEP1-MOD1T.cpp : Defines the entry point for the application.
+//
+
+#include "CEP1-MOD1T.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
