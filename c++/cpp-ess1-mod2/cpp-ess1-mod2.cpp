f3bc61fb1cc523a62e41f1535592d28c12f284bd
nate <costerboy03@outlook.com>
2022-05-17 10:12:17 -0500

    new quiz

---
+++ c++/cpp-ess1-mod2/cpp-ess1-mod2.cpp
@@ -0,0 +1,12 @@
+﻿// cpp-ess1-mod2.cpp : Defines the entry point for the application.
+//
+
+#include "cpp-ess1-mod2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
