0860e39d325625ae65f59e86a42c01f83a55d7eb
nate <costerboy03@outlook.com>
2022-05-17 21:37:47 -0500

    done

--- c++/CEP1-MOD3T/CEP1-MOD3T.cpp
+++ c++/CEP1-MOD3T/CEP1-MOD3T.cpp
@@ -5,8 +5,17 @@
 
 using namespace std;
 
+MyClass sus;
+
 int main()
 {
+	sus.qz1();
+	sus.qz2();
+	sus.qz3();
+	sus.qz4();
+	sus.qz8();
+	sus.qz10();
+	// 100%
 	cout << "Hello CMake." << endl;
 	return 0;
 }
