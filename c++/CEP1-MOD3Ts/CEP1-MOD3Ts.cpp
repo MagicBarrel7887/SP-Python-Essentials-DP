f6748a90d940bf6bf4eea18a650b9ac6f7557bbd
nate <costerboy03@outlook.com>
2022-05-24 11:41:45 -0500

    start

---
+++ c++/CEP1-MOD3Ts/CEP1-MOD3Ts.cpp
@@ -0,0 +1,12 @@
+﻿// CEP1-MOD3Ts.cpp : Defines the entry point for the application.
+//
+
+#include "CEP1-MOD3Ts.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ c++/CEP1-MOD3Ts/CEP1-MOD3Ts.h
@@ -0,0 +1,25 @@
+﻿// CEP1-MOD3Ts.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
+// TODO: Reference additional headers your program requires here.
