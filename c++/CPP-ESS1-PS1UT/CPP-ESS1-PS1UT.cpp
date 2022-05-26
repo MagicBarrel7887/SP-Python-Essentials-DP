327a1935a5ec67ae2eeee2511d68616585872074
nate <costerboy03@outlook.com>
2022-05-26 10:17:44 -0500

    CPP-ESS1-PS1UT start

---
+++ c++/CPP-ESS1-PS1UT/CPP-ESS1-PS1UT.cpp
@@ -0,0 +1,12 @@
+﻿// CPP-ESS1-PS1UT.cpp : Defines the entry point for the application.
+//
+
+#include "CPP-ESS1-PS1UT.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
