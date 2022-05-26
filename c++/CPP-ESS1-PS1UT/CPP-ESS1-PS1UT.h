bbe59bda1bc70ea189af5a7357ede644eb87c076
nate <costerboy03@outlook.com>
2022-05-26 11:15:39 -0500

    pt 8

--- c++/CPP-ESS1-PS1UT/CPP-ESS1-PS1UT.h
+++ c++/CPP-ESS1-PS1UT/CPP-ESS1-PS1UT.h
@@ -4,5 +4,118 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class MyClass
+{
+public:
+	void qz0();
+	void qz1();
+	void qz2();
+	void qz3();
+	void qz4();
+	void qz5();
+	void qz6();
+};
+
+void MyClass::qz0()
+{
+
+	cout << "\n" << endl;
+	cout << "h" << endl;
+	cout << "0" << endl;
+}
+
+void MyClass::qz1()
+{
+	int i = 2, j = i++, k = i++;
+
+	i = 3;
+
+	cout << "\n" << endl;
+	cout << k - i << j - i << endl;
+	cout << "0" << endl;
+}
+
+char do1(char* x) {
+	return *x;
+}
+
+char* do2(char* y)
+{
+	return y;
+}
+char* do3(char& z)
+{
+	return &z;
+}
+
+void MyClass::qz2()
+{
+	char sign = '1';
+
+
+	cout << "\n" << endl;
+	cout << do1(do2(do3(sign))) << endl;
+	cout << "2" << endl;
+}
+
+void MyClass::qz3()
+{
+	bool b1 = !true;
+	bool b2 = !b1 && false;
+	bool b3 = b2 || true;
+
+	if (b3)
+		cout << "T" << endl;
+	else
+		cout << "F" << endl;
+
+	cout << "\n" << endl;
+	cout << "h" << endl;
+	cout << "3" << endl;
+}
+
+void MyClass::qz4()
+{
+	string s = "123";
+	s.append(s.substr(2)).push_back(s[s.length() - 2]);
+
+	cout << "\n" << endl;
+	cout << s << endl;
+	cout << "4" << endl;
+}
+
+float combine(float x1 = 0.0, int x2 = 1.0) {
+	return x2 + x1;
+}
+
+void MyClass::qz5()
+{
+	cout << "\n" << endl;
+	cout << combine() + combine(1.) + combine(2.,3.) << endl;
+	cout << "\n" << endl;
+	cout << "h" << endl;
+	cout << "5" << endl;
+}
+
+void MyClass::qz6()
+{
+	int k = 3;
+	if (k > 0) {
+		if (k != 3)
+			k--;
+		if (k == 3)
+			k++;
+	}
+	if (k < 0) {
+		k = 5;
+	}
+	cout << "\n" << endl;
+	cout << k << endl;
+	cout << "6" << endl;
+}
 
 // TODO: Reference additional headers your program requires here.
